5927f70cfccb7fdab59e09b84fcb79011610f2a8
Matheus Paula Frazão <matheus_paula2012@hotmail.com>
2022-06-04 18:52:16 -0300

    array types changed

--- start/socket.ts
+++ start/socket.ts
@@ -1,11 +1,11 @@
 import Ws from 'App/Services/Ws'
 
-const messages = []
+const messages: string[] = []
 
 Ws.start((socket) => {
   console.log(socket.id)
   socket.emit('updatechat', messages)
-  socket.on('chatmessage', (data) => {
+  socket.on('chatmessage', (data: string) => {
     console.log(messages)
     messages.push(data)
     console.log(data)
