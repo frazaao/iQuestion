c03831ce86cd78c2e4ace5a087943822ceb1ba86
Matheus Paula Frazão <matheus_paula2012@hotmail.com>
2022-08-07 15:24:07 -0300

    Teste Websockets

--- start/socket.ts
+++ start/socket.ts
@@ -3,10 +3,10 @@ import Ws from 'App/Services/Ws'
 const messages: string[] = []
 
 Ws.start((socket) => {
-  socket.on('create', (room) => {
-    console.log('sala criada', room)
-    socket.join(room)
-  })
+  // socket.on('create', (room) => {
+  //   console.log('sala criada', room)
+  //   socket.join(room)
+  // })
   socket.emit('updatechat', messages)
   socket.on('chatmessage', (data: string) => {
     console.log(messages)
