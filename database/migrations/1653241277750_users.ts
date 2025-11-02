bdc21a77805ae6dfdba824fd85bb4360b3d80817
Matheus Paula Frazão <matheus_paula2012@hotmail.com>
2025-11-02 15:03:21 -0300

    Change migrations

--- database/migrations/1653241277750_users.ts
+++ database/migrations/1653241277750_users.ts
@@ -15,8 +15,8 @@ export default class UsersSchema extends BaseSchema {
       /**
        * Uses timestampz for PostgreSQL and DATETIME2 for MSSQL
        */
-      table.timestamp('created_at', { useTz: true }).notNullable()
-      table.timestamp('updated_at', { useTz: true }).notNullable()
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true }).nullable()
     })
   }
 
